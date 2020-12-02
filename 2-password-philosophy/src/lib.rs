7d0d72a55d1317f1d2a8640b90fa7c366b71e505
Corey Alexander <coreyja@gmail.com>
2020-12-02 00:35:46 -0500

    Part 2 done

--- 2-password-philosophy/src/lib.rs
+++ 2-password-philosophy/src/lib.rs
@@ -1,12 +1,27 @@
-pub fn count_valid_passwords(input: &str) -> usize {
+pub fn count_valid_passwords_part_a(input: &str) -> usize {
     input
         .trim()
         .split("\n")
-        .filter(|&x| is_valid_password(x))
+        .filter(|&x| is_valid_password_part_a(x))
         .count()
 }
 
-fn is_valid_password(input: &str) -> bool {
+pub fn count_valid_passwords_part_b(input: &str) -> usize {
+    input
+        .trim()
+        .split("\n")
+        .filter(|&x| is_valid_password_part_b(x))
+        .count()
+}
+
+struct PasswordLine {
+    num_1: usize,
+    num_2: usize,
+    c: char,
+    password: String,
+}
+
+fn parse_line(input: &str) -> PasswordLine {
     let mut i = input.split(":");
     let first_half = i.next().unwrap().trim();
     let mut first_half_i = first_half.split(" ");
@@ -19,22 +34,57 @@ fn is_valid_password(input: &str) -> bool {
 
     let password = i.next().unwrap().trim();
 
-    let count = password.chars().filter(|&x| x == char_to_count).count();
+    PasswordLine {
+        num_1: min,
+        num_2: max,
+        c: char_to_count,
+        password: password.to_owned(),
+    }
+}
 
-    count >= min && count <= max
+fn is_valid_password_part_a(input: &str) -> bool {
+    let parsed = parse_line(input);
+
+    let count = parsed.password.chars().filter(|&x| x == parsed.c).count();
+
+    count >= parsed.num_1 && count <= parsed.num_2
+}
+
+fn is_valid_password_part_b(input: &str) -> bool {
+    let parsed = parse_line(input);
+
+    let pass_chars: Vec<_> = parsed.password.chars().collect();
+
+    (pass_chars[parsed.num_1 - 1] == parsed.c) ^ (pass_chars[parsed.num_2 - 1] == parsed.c)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::count_valid_passwords;
+    use crate::{count_valid_passwords_part_a, count_valid_passwords_part_b};
 
     #[test]
     fn it_works_for_the_sample_input() {
-        assert_eq!(count_valid_passwords(include_str!("sample.input")), 2);
+        assert_eq!(
+            count_valid_passwords_part_a(include_str!("sample.input")),
+            2
+        );
     }
 
     #[test]
     fn it_works_for_my_input() {
-        assert_eq!(count_valid_passwords(include_str!("my.input")), 477);
+        assert_eq!(count_valid_passwords_part_a(include_str!("my.input")), 477);
+    }
+
+    #[test]
+    fn part_b_it_works_for_the_sample_input() {
+        assert_eq!(
+            count_valid_passwords_part_b(include_str!("sample.input")),
+            1
+        );
+    }
+
+    #[test]
+    fn part_b_it_works_for_my_input() {
+        assert_eq!(count_valid_passwords_part_b(include_str!("my.input")), 686);
     }
 }
