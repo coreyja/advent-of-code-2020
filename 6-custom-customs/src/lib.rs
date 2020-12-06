8838ec41e64b09080e16e34a86322e2987fa72e3
Corey Alexander <coreyja@gmail.com>
2020-12-06 15:05:04 -0500

    Sample input part b

--- 6-custom-customs/src/lib.rs
+++ 6-custom-customs/src/lib.rs
@@ -20,6 +20,29 @@ pub fn sum_any_yes_questions(input: &str) -> usize {
         .sum()
 }
 
+fn count_number_of_all_yes_questions(group: &str) -> usize {
+    if group.lines().count() > 0 {
+        let yes_questions: HashSet<char> = (b'a'..=b'z').map(char::from).collect();
+
+        group
+            .lines()
+            .fold(yes_questions, |acc, x| {
+                acc.intersection(&x.chars().collect()).cloned().collect()
+            })
+            .len()
+    } else {
+        0
+    }
+}
+
+pub fn sum_all_yes_questions(input: &str) -> usize {
+    input
+        .trim()
+        .split("\n\n")
+        .map(|x| count_number_of_all_yes_questions(x))
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use crate::*;
@@ -45,5 +68,23 @@ ac"
     #[test]
     fn part_a_works_for_my_input() {
         assert_eq!(sum_any_yes_questions(include_str!("my.input")), 6735)
+    }
+
+    #[test]
+    fn sample_groups_work_part_b() {
+        assert_eq!(count_number_of_all_yes_questions(""), 0);
+        assert_eq!(count_number_of_all_yes_questions("abc"), 3);
+        assert_eq!(
+            count_number_of_all_yes_questions(
+                "ab
+ac"
+            ),
+            1
+        );
+    }
+
+    #[test]
+    fn part_b_works_for_sample_input() {
+        assert_eq!(sum_all_yes_questions(include_str!("sample.input")), 6)
     }
 }
