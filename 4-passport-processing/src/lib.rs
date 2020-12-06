47add512a62ffdc2f2c211bcf64a25fdb087f1d5
Corey Alexander <coreyja@gmail.com>
2020-12-05 23:50:58 -0500

    Refactor to make part b easy

--- 4-passport-processing/src/lib.rs
+++ 4-passport-processing/src/lib.rs
@@ -1,44 +1,115 @@
-type Passport<'a> = std::collections::HashMap<&'a str, &'a str>;
+use std::str::FromStr;
 
-pub fn count_valid_passports(input: &str, required_keys: &Vec<&str>) -> usize {
+struct Passport {
+    byr: Option<String>,
+    iyr: Option<String>,
+    eyr: Option<String>,
+    hgt: Option<String>,
+    hcl: Option<String>,
+    ecl: Option<String>,
+    pid: Option<String>,
+    cid: Option<String>,
+}
+
+impl Passport {
+    pub fn valid(&self) -> bool {
+        self.byr_valid()
+            && self.iyr_valid()
+            && self.eyr_valid()
+            && self.hgt_valid()
+            && self.hcl_valid()
+            && self.ecl_valid()
+            && self.pid_valid()
+    }
+
+    pub fn has_all_required_fields(&self) -> bool {
+        self.byr.is_some()
+            && self.iyr.is_some()
+            && self.eyr.is_some()
+            && self.hgt.is_some()
+            && self.hcl.is_some()
+            && self.ecl.is_some()
+            && self.pid.is_some()
+    }
+
+    fn byr_valid(&self) -> bool {
+        true
+    }
+
+    fn iyr_valid(&self) -> bool {
+        true
+    }
+
+    fn eyr_valid(&self) -> bool {
+        true
+    }
+
+    fn hgt_valid(&self) -> bool {
+        true
+    }
+
+    fn hcl_valid(&self) -> bool {
+        true
+    }
+
+    fn ecl_valid(&self) -> bool {
+        true
+    }
+
+    fn pid_valid(&self) -> bool {
+        true
+    }
+}
+
+impl FromStr for Passport {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        fn from_map(hash: &std::collections::HashMap<&str, &str>, key: &str) -> Option<String> {
+            hash.get(key).map(|&x| x.to_owned())
+        }
+
+        let mut hash = std::collections::HashMap::new();
+
+        input.split_whitespace().for_each(|x| {
+            let mut iter = x.split(":");
+
+            let key = iter.next().expect("key to exist");
+            let value = iter.next().expect("value to exist");
+
+            hash.insert(key, value);
+        });
+
+        Ok(Self {
+            byr: from_map(&hash, "byr"),
+            iyr: from_map(&hash, "iyr"),
+            eyr: from_map(&hash, "eyr"),
+            hgt: from_map(&hash, "hgt"),
+            hcl: from_map(&hash, "hcl"),
+            ecl: from_map(&hash, "ecl"),
+            pid: from_map(&hash, "pid"),
+            cid: from_map(&hash, "cid"),
+        })
+    }
+}
+
+pub fn count_passports_with_all_fields(input: &str) -> usize {
     input
         .trim()
         .split("\n\n")
-        .map(|x| parse_passport(x))
-        .filter(|x| valid_password(x, required_keys))
+        .map(|x| Passport::from_str(x).unwrap())
+        .filter(|x| x.has_all_required_fields())
         .count()
-}
-
-fn valid_password(passport: &Passport, required_keys: &Vec<&str>) -> bool {
-    required_keys.iter().all(|key| passport.contains_key(key))
-}
-
-fn parse_passport(input: &str) -> Passport {
-    let mut passport = Passport::new();
-
-    input.split_whitespace().for_each(|x| {
-        let mut iter = x.split(":");
-
-        let key = iter.next().expect("key to exist");
-        let value = iter.next().expect("value to exist");
-
-        passport.insert(key, value);
-    });
-
-    passport
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::count_valid_passports;
+    use crate::count_passports_with_all_fields;
 
     #[test]
     fn part_a_works_for_sample_input() {
         assert_eq!(
-            count_valid_passports(
-                include_str!("sample.input"),
-                &vec!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-            ),
+            count_passports_with_all_fields(include_str!("sample.input"),),
             2
         );
     }
@@ -46,10 +117,7 @@ mod tests {
     #[test]
     fn part_a_works_for_my_input() {
         assert_eq!(
-            count_valid_passports(
-                include_str!("my.input"),
-                &vec!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-            ),
+            count_passports_with_all_fields(include_str!("my.input"),),
             222
         );
     }
